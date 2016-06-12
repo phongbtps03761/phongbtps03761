cd095d71dcc26ffcff2d0714b6705debd0ddf6d1
phongbtps03761 <phongbtps03761@fpt.edu.vn>
2016-06-12 15:50:48 +0700

    demo lab6

---
+++ QLBH/frmLoaiSanPham.Designer.vb
@@ -0,0 +1,144 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmLoaiSanPham
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.btnThem = New System.Windows.Forms.Button()
+        Me.btnSua = New System.Windows.Forms.Button()
+        Me.btnXoa = New System.Windows.Forms.Button()
+        Me.btnTim = New System.Windows.Forms.Button()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(13, 13)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(93, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Mã loại sản phẩm:"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(113, 10)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(140, 20)
+        Me.TextBox1.TabIndex = 1
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(13, 54)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(97, 13)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "Tên loại sản phẩm:"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(113, 51)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(140, 20)
+        Me.TextBox2.TabIndex = 1
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(291, 10)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(240, 239)
+        Me.DataGridView1.TabIndex = 2
+        '
+        'btnThem
+        '
+        Me.btnThem.Location = New System.Drawing.Point(16, 108)
+        Me.btnThem.Name = "btnThem"
+        Me.btnThem.Size = New System.Drawing.Size(109, 37)
+        Me.btnThem.TabIndex = 3
+        Me.btnThem.Text = "Thêm"
+        Me.btnThem.UseVisualStyleBackColor = True
+        '
+        'btnSua
+        '
+        Me.btnSua.Location = New System.Drawing.Point(144, 108)
+        Me.btnSua.Name = "btnSua"
+        Me.btnSua.Size = New System.Drawing.Size(109, 37)
+        Me.btnSua.TabIndex = 3
+        Me.btnSua.Text = "Sữa"
+        Me.btnSua.UseVisualStyleBackColor = True
+        '
+        'btnXoa
+        '
+        Me.btnXoa.Location = New System.Drawing.Point(16, 168)
+        Me.btnXoa.Name = "btnXoa"
+        Me.btnXoa.Size = New System.Drawing.Size(109, 37)
+        Me.btnXoa.TabIndex = 3
+        Me.btnXoa.Text = "Xóa"
+        Me.btnXoa.UseVisualStyleBackColor = True
+        '
+        'btnTim
+        '
+        Me.btnTim.Location = New System.Drawing.Point(144, 168)
+        Me.btnTim.Name = "btnTim"
+        Me.btnTim.Size = New System.Drawing.Size(109, 37)
+        Me.btnTim.TabIndex = 3
+        Me.btnTim.Text = "Tìm kiếm"
+        Me.btnTim.UseVisualStyleBackColor = True
+        '
+        'frmLoaiSanPham
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(574, 261)
+        Me.Controls.Add(Me.btnTim)
+        Me.Controls.Add(Me.btnSua)
+        Me.Controls.Add(Me.btnXoa)
+        Me.Controls.Add(Me.btnThem)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "frmLoaiSanPham"
+        Me.Text = "frmLoaiSanPham"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents btnThem As Button
+    Friend WithEvents btnSua As Button
+    Friend WithEvents btnXoa As Button
+    Friend WithEvents btnTim As Button
+End Class
---
+++ QLBH/frmSanPham.vb
@@ -0,0 +1,65 @@
+﻿Imports System.Data.DataSet
+Imports System.Data.SqlClient
+
+
+Public Class frmSanPham
+    Dim duongdan As String
+    Dim db As New DataTable
+    Dim chuoiketnoi As String = "Data Source=DESKTOP-OUKJKS0;Initial Catalog=PS03761_INF205;Integrated Security=True"
+    Dim conn As SqlConnection = New SqlConnection(chuoiketnoi)
+    Private DBAccess As DataBaseAccess
+    'dinh nghia ham them san pham vao dtb
+    Private Function themsp() As Boolean
+        Dim sqlQuery As String = "INSERT INTO SANPHAM(MA_SP,TEN_SP,GIA,MA_LOAI)"
+        sqlQuery += String.Format("VALUES ('{0}','{1}','{2}','{3}')", txbMaSP.Text, txbTenSP.Text, txbDonGia.Text, txbMaLoai.Text)
+        Return DBAccess.ExecuteNoneQuery(sqlQuery)
+    End Function
+    'dinh nghia ham kiem tra gia tri trươc khi them
+    Private Function isnull() As Boolean
+        Return (String.IsNullOrEmpty(txbMaSP.Text) OrElse String.IsNullOrEmpty(txbTenSP.Text) OrElse String.IsNullOrEmpty(txbDonGia.Text) OrElse String.IsNullOrEmpty(txbMaLoai.Text))
+    End Function
+    'dinh nghia ham tim kiem
+    Private Sub timsp(value As String)
+        Dim sqlQuery As String = String.Format("select MA_SP,TEN_SP,GIA,MA_LOAI FROM dbo.SANPHAM")
+        If Me.cobTim.SelectedIndex = 0 Then 'tim theo ma sp
+            sqlQuery += String.Format("Where MA_SP like '{0}'%", value)
+        ElseIf Me.cobTim.SelectedIndex = 1 Then 'tim theo ten sp
+            sqlQuery += String.Format("WHere TEN_SP like '{0}'%", value)
+        End If
+        Dim dtable As DataTable = DBAccess.GetDataTable(sqlQuery)
+        Me.dgvSanPham.DataSource = dtable
+        With Me.dgvSanPham
+            .Columns(0).HeaderText = "MA_SP"
+            .Columns(1).HeaderText = "TEN_SP"
+            .Columns(2).HeaderText = "GIA"
+            .Columns(3).HeaderText = "MA_LOAI"
+        End With
+    End Sub
+
+    Private Sub frmSanPham_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim sqlQuery As String = String.Format("select MA_SP,TEN_SP,GIA,MA_LOAI FROM dbo.SANPHAM")
+        Dim dtable As DataTable = DBAccess.GetDataTable(sqlQuery)
+        Me.dgvSanPham.DataSource = dtable
+        With Me.dgvSanPham
+            .Columns(0).HeaderText = "MA_SP"
+            .Columns(1).HeaderText = "TEN_SP"
+            .Columns(2).HeaderText = "GIA"
+            .Columns(3).HeaderText = "MA_LOAI"
+        End With
+    End Sub
+
+    Private Sub btnThem_Click(sender As Object, e As EventArgs) Handles btnThem.Click
+        If isnull() Then
+            MessageBox.Show("Thieu du lieu", "Error", MessageBoxButtons.OK)
+        Else
+            If themsp() Then
+                MessageBox.Show("Them du lieu thanh cong", "OK", MessageBoxButtons.OK)
+                DialogResult = DialogResult.OK
+            Else
+                MessageBox.Show("loi", "error", MessageBoxButtons.OK)
+                DialogResult = DialogResult.OK
+            End If
+            timsp(Me.txbTim.Text)
+        End If
+    End Sub
+End Class
